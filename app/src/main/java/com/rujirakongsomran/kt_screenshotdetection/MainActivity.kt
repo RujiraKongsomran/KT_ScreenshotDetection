a4c9c3081c3ed55045587bb5c1d69c2a13007d89
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-05-27 19:57:24 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_screenshotdetection/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_screenshotdetection/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.rujirakongsomran.kt_screenshotdetection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.rujirakongsomran.kt_screenshotdetection.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ScreenshotDetectionActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    override fun onScreenCaptured(path: String) {
+        Snackbar.make(binding.root, path, Snackbar.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/rujirakongsomran/kt_screenshotdetection/ScreenshotDetectionActivity.kt
@@ -0,0 +1,85 @@
+package com.rujirakongsomran.kt_screenshotdetection
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.akexorcist.screenshotdetection.ScreenshotDetectionDelegate
+
+open class ScreenshotDetectionActivity : AppCompatActivity(),
+    ScreenshotDetectionDelegate.ScreenshotDetectionListener {
+
+    companion object {
+        private const val REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION = 3009
+    }
+
+    private val screenshotDetectionDelegate = ScreenshotDetectionDelegate(this, this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkReadExternalStoragePermission()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        screenshotDetectionDelegate.startScreenshotDetection()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        screenshotDetectionDelegate.stopScreenshotDetection()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION -> {
+                if (grantResults.getOrNull(0) == PackageManager.PERMISSION_DENIED) {
+                    showReadExternalStoragePermissionDeniedMessage()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    override fun onScreenCaptured(path: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScreenCapturedWithDeniedPermission() {
+        TODO("Not yet implemented")
+    }
+
+    private fun checkReadExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestReadExternalStoragePermission()
+        }
+    }
+
+    private fun requestReadExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION
+        )
+    }
+
+    private fun showReadExternalStoragePermissionDeniedMessage() {
+        Toast.makeText(
+            this,
+            R.string.read_external_storage_permission_denied_message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
